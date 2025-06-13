37eb45e1c18c381e9f316c068c52faced1bf77a5
The Trident <95124749+TheatreVietNam@users.noreply.github.com>
2025-06-13 12:38:54 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCBw-FyGPczORqZ_ZN-LboysmAxnsbhzmY",
+  authDomain: "the-dec0a.firebaseapp.com",
+  databaseURL: "https://the-dec0a-default-rtdb.firebaseio.com",
+  projectId: "the-dec0a",
+  storageBucket: "the-dec0a.appspot.com",
+  messagingSenderId: "1011515436985",
+  appId: "1:1011515436985:web:074516656d82ce1a3f9068"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
